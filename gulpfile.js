7547522a7a95cf75e61a62c3ecfa68e91ddc7b81
ermia <afsharianarmia@gmail.com>
2019-11-25 19:19:30 -0800

    fix getting category data bug

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,25 @@ const gulp = require("gulp"); // Load Gulp!
 // Now that we've installed the terser package we can require it:
 const terser = require("gulp-terser"),
   rename = require("gulp-rename"),
-  eslint = require("gulp-eslint");
+  eslint = require("gulp-eslint"),
+  sass= require('gulp-sass'),
+  cssnano= require('gulp-cssnano'),
+  autoprefixer= require('gulp-autoprefixer');
 
   const browserSync = require('browser-sync').create();
 
+gulp.task('sass',function () {
+  return gulp
+  .src ('./sass/main.scss')
+  .pipe(sass())
+  .pipe(cssnano())
+  .pipe(autoprefixer ({
+    browsers: ["last 2 versions"]
+  }) )
+  .pipe(cssnano())
+  .pipe(rename("style.min.css"))
+  .pipe(gulp.dest('./build/css'))
+})
 gulp.task("scripts", function() {
   return gulp
     .src("./js/*.js") // What files do we want gulp to consume?
@@ -31,6 +46,7 @@ gulp.task('reload',function(done) {
 })
 gulp.task("watch", function() {
   gulp.watch("js/*.js", gulp.series("scripts"));
+  gulp.watch("sass/*.scss", gulp.series("sass", 'reload'));
   gulp.watch("./index.html",gulp.series("reload"));
 
 
--- js/index.js
+++
@@ -1,7 +0,0 @@
-const knrkner=23;
-const jkl= 234;
-function name() {
-
-    const car= knrkner ;
-    console.log(car)
-}
